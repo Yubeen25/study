68b9f174f53a7b188a98c19ef2e906f5fbf182c3
Yubeen25 <122802925+Yubeen25@users.noreply.github.com>
2023-02-27 21:40:24 +0900

    Add files via upload

---
+++ k번째 수.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  p
+//
+//  Created by 진유빈 on 2023/02/27.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for(int i=0; i < commands.size() ; i++){
+        vector<int> cut_array;
+        for(int index = commands[i][0]-1; index <= commands[i][1]-1; index++){
+            cut_array.push_back(array[index]);
+        }
+        sort(cut_array.begin(), cut_array.end());
+        answer.push_back(cut_array[commands[i][2]-1]);
+    }
+    
+    return answer;
+}
